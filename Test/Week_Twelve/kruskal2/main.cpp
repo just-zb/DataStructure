f5f10022557b526222277d2f4c1b771644d57696
ZhuBao <zhubao@mail.nankai.edu.cn>
2020-12-01 10:42:19 +0800

    更新了数据结构第十二周上课讲的算法代码

--- Test/Week_Twelve/kruskal2/main.cpp
+++ Test/Week_Twelve/kruskal2/main.cpp
@@ -4,7 +4,7 @@ class nevermind{
     int* parents;
     // 初始化
 public:
-    nevermind(int n){
+    nevermind(int n){//在初始化时，让所有节点的父节点都是它本身
         parents = new int[n];
         for(int i=0;i<n;i++){
             parents[i] = i;
@@ -12,15 +12,15 @@ public:
     }
 
     // 合并
-    void unionkey(int node1,int node2){
+    void unionkey(int node1,int node2){//组合两个村庄，让两个村庄在同一个集合
         int root1 = find(node1);
         int root2 = find(node2);
-        if(root1!=root2){
+        if(root1!=root2){//如果node1的祖宗节点不是node2的祖宗节点，那么让node2的祖宗节点指向node1的祖父节点
             parents[root2] = root1;
         }
     };
     // 查找
-    int find(int node){
+    int find(int node){//找到该节点的祖父节点，只有祖父节点的父节点是祖父节点本身
         while(parents[node]!=node){
             parents[node] = parents[parents[node]];
             node = parents[node];
@@ -29,10 +29,11 @@ public:
     }
 
     // 看是否在同一个集合
-bool isConnected(int node1,int node2){
+bool isConnected(int node1,int node2){//观察两个节点的祖宗节点是否相同，只有相同了之后才能连通
         return find(node1) == find(node2);
     };
 };
+//在这里展现出并查集的代表（就是根节点的重要性，这种方法为什么能解决村庄联通的问题？）
 int main() {
     int m,n;
     cin>>m>>n;
